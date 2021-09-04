4b7fd10c0a56749c664e11ead4a3573443efe85d
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-09-04 18:52:42 +0200

    Remove .js endings

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import Creator from '../Creator/Creator.js';
 import {settings} from '../../data/dataStore';
-import Icon from '../Icon/Icon.js'
+import Icon from '../Icon/Icon'
 
 class Column extends React.Component {
     
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Hero from '../Hero/Hero.js';
+import Hero from '../Hero/Hero';
 import styles from './List.scss';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+import Column from '../Column/Column';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator';
 
 
 class List extends React.Component {
