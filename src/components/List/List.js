cae1f2912e95bc859fabf9c4a2df666a5d9f1fc7
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-09-05 21:17:54 +0200

    Working on Redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Hero from '../Hero/Hero';
 import styles from './List.scss';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column';
+// import Column from '../Column/Column';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator';
+// import Creator from '../Creator/Creator';
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  };
+  // state = {
+  //   columns: this.props.columns || [],
+  // };
 
   static propTypes = {
     title: PropTypes.node.isRequired,
@@ -23,29 +23,30 @@ class List extends React.Component {
     description: ReactHtmlParser(settings.defaultListDescription),
   };
 
-  addColumn(title) {
-    console.log('this.props.columns', this.props.columns);
-    this.setState((state) => ({
-      columns: [
-        ...state.columns,
-        {
-          key: state.columns.length
-            ? state.columns[state.columns.length - 1].key + 1
-            : 0,
-          title,
-          icon: 'list-alt',
-          cards: [],
-        },
-      ],
-    }));
-  }
+  // addColumn(title) {
+  //   console.log('this.props.columns', this.props.columns);
+  //   this.setState((state) => ({
+  //     columns: [
+  //       ...state.columns,
+  //       {
+  //         key: state.columns.length
+  //           ? state.columns[state.columns.length - 1].key + 1
+  //           : 0,
+  //         title,
+  //         icon: 'list-alt',
+  //         cards: [],
+  //       },
+  //     ],
+  //   }));
+  // }
 
   render() {
+    const { title, image, description } = this.props;
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} mainImage={this.props.image} />
-        <div className={styles.description}>{this.props.description}</div>
-        <div className={styles.columns}>
+        <Hero titleText={title} mainImage={image} />
+        <div className={description}>{ReactHtmlParser(description)}</div>
+        {/* <div className={styles.columns}>
           {this.state.columns.map(({ key, ...columnProps }) => (
             <Column key={key} {...columnProps} />
           ))}
@@ -55,7 +56,7 @@ class List extends React.Component {
             text={settings.columnCreatorText}
             action={(title) => this.addColumn(title)}
           />
-        </div>
+        </div> */}
       </section>
     );
   }
